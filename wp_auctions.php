529f352fbec52d4f69098c1bfbf36f54cd1d840b
owencutajar <owencutajar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-08 12:12:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-auctions/trunk@143974 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_auctions.php
+++ wp_auctions.php
@@ -6,6 +6,20 @@ Description: Implements the ability to run auctions on your own blog. Once activ
 Version: 1.0.6
 Author: Owen Cutajar & Hyder Jaffari
 Author URI: http://www.wpauctions.com/profile
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
   /* History:
